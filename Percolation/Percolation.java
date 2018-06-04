45c4460c33095d82c93e885a6e346faf0b671ea2
Leslie <jinl0ng@icloud.com>
2018-06-04 22:21:36 +0800

    add Percolation.java

---
+++ Percolation/Percolation.java
@@ -0,0 +1,45 @@
+/*----------------------------------------------------------------
+ *  Author:        Jinlong Zhu
+ *  Written:       2018/06/04
+ *  Last updated:  2018/06/04
+ *
+ *  Compilation:   javac-algs4 Percolation.java
+ *  Execution:     java-algs4 Percolation
+ *  
+ *  This is part of the Algorithms, Part I on coursera programing assignment.
+ *  Programming Assignment 1: Percolation
+ *  See more: http://coursera.cs.princeton.edu/algs4/assignments/percolation.html
+ *
+ *
+ *----------------------------------------------------------------*/
+
+  /*--------------------------------------------------------------
+  *  API lists:
+  *  create n-by-n grid, with all sites blocked
+  *  public Percolation(int n);
+  *
+  *  open site (row, col) if it is not open already
+  *  public    void open(int row, int col);
+  *
+  *  is site (row, col) open?
+  *  public boolean isOpen(int row, int col);
+  *
+  *  is site (row, col) full?
+  *  public boolean isFull(int row, int col);
+  *
+  *  number of open sites
+  *  public     int numberOfOpenSites();
+  *
+  *  does the system percolates?
+  *  public boolean percolates();
+  *---------------------------------------------------------------*/
+
+
+public class Percolation {
+
+  public Percolation(int n){
+    
+  }
+
+  public static void main(String[] args){}
+}
